a50250e8f1cd1a45f51b46b0799ca3bd6cc7a68d
MohammadAsalmUddin <aslamuddin0015@gmail.com>
2021-03-02 14:22:30 +0600

    #List in Viewstate #List of data sending from one method to another method

---
+++ FourthUI.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FourthUI.aspx.cs" Inherits="WebFormApplication.FourthUI" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Name"></asp:Label>
+            <asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="saveButton" runat="server" OnClick="saveButton_Click" Text="Save" />
+            <br />
+            <asp:Button ID="showButton" runat="server" OnClick="showButton_Click" Text="Show" />
+            <br />
+            <asp:ListBox ID="nameListBox" runat="server" Height="247px" Width="149px"></asp:ListBox>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Total Count"></asp:Label>
+            <asp:TextBox ID="totalCountTextBox" runat="server"></asp:TextBox>
+        </div>
+    </form>
+</body>
+</html>
---
+++ FourthUI.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormApplication
+{
+    public partial class FourthUI : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void saveButton_Click(object sender, EventArgs e)
+        {
+            string name = nameTextBox.Text;
+            if (ViewState["Names"] == null)
+            {
+                List<string> names = new List<string>();
+                names.Add(name);
+                ViewState["Names"] = names;
+            }
+            else
+            {
+                List<string> names = (List<string>) ViewState["Names"];
+                names.Add(name);
+                ViewState["names"] = names;
+            }
+            nameTextBox.Text = String.Empty;
+        }
+
+
+        protected void showButton_Click(object sender, EventArgs e)
+        {
+            nameListBox.Items.Clear();
+            List<string> nameList = (List<string>) ViewState["Names"];
+
+            foreach (var name in nameList)
+            {
+                nameListBox.Items.Add(name);
+            }
+
+            totalCountTextBox.Text = nameList.Count.ToString();
+        }
+    }
+}
