ee314aee7bd486b886b6ff8a57bb7c19f321ee6d
MohammadAsalmUddin <aslamuddin0015@gmail.com>
2021-03-02 13:42:34 +0600

    #ViewState #Diff Between Web Form and Windows Form #Web App use Viewstate to use another functions object #It's load the page at every single instructions #if the page loads again and again the result will be null

---
+++ ThirdUI.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormApplication
+{
+    public partial class ThirdUI : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void saveButton_Click(object sender, EventArgs e)
+        {
+            string name = nameTextBox.Text;
+            ViewState["name"] = name;
+
+            nameTextBox.Text = String.Empty;
+            
+        }
+
+        protected void showButton_Click(object sender, EventArgs e)
+        {
+            string name = ViewState["name"].ToString();
+            resultLabel.Text = name;
+        }
+    }
+}
